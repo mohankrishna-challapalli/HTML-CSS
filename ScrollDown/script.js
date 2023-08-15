6596c94f7e9974cd8f2a9fb100a1dbd9a0520ec0
mohankrishna-challapalli <mohankrishna.challapalli@gmail.com>
2023-08-15 10:35:29 +0530

    Completed the documentation for the existinig code

--- ScrollDown/script.js
+++ ScrollDown/script.js
@@ -1,14 +1,20 @@
+// Wait for the DOM to be fully loaded before executing the script
 document.addEventListener("DOMContentLoaded", function () {
+  // Select the scroll down button element
   const scrollDownBtn = document.querySelector(".scroll-down-btn");
 
+  // Add a click event listener to the scroll down button
   scrollDownBtn.addEventListener("click", function () {
+    // Select the content section element
     const content = document.querySelector(".content");
+
+    // Get the top offset of the content section relative to the document
     const contentOffsetTop = content.offsetTop;
 
     // Smooth scroll to the content section
     window.scrollTo({
       top: contentOffsetTop,
-      behavior: "smooth",
+      behavior: "smooth", // Apply smooth scrolling behavior
     });
   });
 });
